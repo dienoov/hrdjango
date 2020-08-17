bc25546edcf59eb87441c9c04cbb3814e0d2548d
dienoov <alvinodien@yahoo.co.id>
2020-08-17 07:20:55 +0700

    Add login page

--- hrdjango/urls.py
+++ hrdjango/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="dashboard"),
+    path("login/", views.login, name="login"),
     path("employees/", include("employees.urls")),
 ]
